e8211269eb4b89762804a4e8b721b9d5d5541aa6
Max Pobedinsky <95541283+Pobedinsky@users.noreply.github.com>
2024-01-06 14:23:31 +0000

    Update login_page.dart

--- login/login_page.dart
+++ login/login_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import '../Manager.dart';
 import '../Utils.dart';
-import '../services/authentication.dart';
-
 
 class LoginPage extends StatefulWidget {
+
   @override
   State<LoginPage>  createState() => _LoginPageState();
 }
@@ -11,19 +11,14 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final Authentication _auth = Authentication();
-
-  Future<void> checkIfLogged() async {
-    await _auth.init();
-    if(_auth.userid!= null){
-      handleLoginResult("success");
-    }
-
-  }
 
   @override
   Widget build(BuildContext context) {
-    checkIfLogged();
+    if (Manager.auth.userid != null) {
+      Future.delayed(Duration.zero, () {
+        Navigator.pushReplacementNamed(context, "/home");
+      });
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login Page'),
@@ -35,7 +30,7 @@ class _LoginPageState extends State<LoginPage> {
           children: [
             TextField(
               controller: _usernameController,
-              decoration: const InputDecoration(labelText: 'Username'),
+              decoration: const InputDecoration(labelText: 'Email'),
             ),
             const SizedBox(height: 16),
             TextField(
@@ -46,8 +41,7 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(height: 24),
             ElevatedButton(
               onPressed: () async {
-                await _auth.init();
-                dynamic result = await _auth.logInUser(
+                dynamic result = await Manager.logInManager(
                   email: _usernameController.text,
                   password: _passwordController.text,
                 );
@@ -57,9 +51,7 @@ class _LoginPageState extends State<LoginPage> {
             ),
             const SizedBox(height: 8),
             TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
-              },
+              onPressed: () { Navigator.pushNamed(context, '/register'); },
               child: const Text('Register'),
             ),
           ],
@@ -69,16 +61,6 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   void handleLoginResult(dynamic result) {
-    /*
-    var aux = FileEditor(
-      name: "page1.ml",
-      language: "ocaml",
-      code: "let () = print_string \"Hello world\"",
-    );
-    var test = DatabaseService(uid: 'ZX9wM4XZrXbKJxugXA6Er1tXswj2');
-    test.uploadFile(aux);
-    */
-
     if (result == 'success') {
       Navigator.pushReplacementNamed(context, "/home");
     } else {
@@ -86,86 +68,3 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 }
-
-
-/*
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../Utils.dart';
-import '../home/home.dart';
-import '../services/authentication.dart';
-import '../Utils.dart';
-
-
-
-class LoginPage extends StatefulWidget {
-
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  final Authentication _auth = Authentication();
-
-
-  @override
-  Widget build(BuildContext context) {
-
-
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(labelText: 'Username'),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () async {
-
-
-                  dynamic result = await _auth.logInUser(
-                      email: _usernameController.text,
-                      password: _passwordController.text);
-                  if(result == 'success'){
-                    Navigator.pushNamed(context, "/home");
-                  }else {
-                    newDialog(context, result, "Attention");
-                  }
-
-
-              },
-              child: const Text('Login'),
-            ),
-            const SizedBox(height: 8),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
-              },
-              child: const Text('Register'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
- */
