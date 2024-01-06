9d75bfe5e2d5874a4d4dd832f0eca9d317b87c1b
Max Pobedinsky <95541283+Pobedinsky@users.noreply.github.com>
2024-01-06 14:24:13 +0000

    Update signup_page.dart

--- login/signup_page.dart
+++ login/signup_page.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
-
+import '../Manager.dart';
 import '../Utils.dart';
-import '../services/authentication.dart';
-
 
 class RegisterPage extends StatefulWidget {
   @override
-  _RegisterPageState createState() => _RegisterPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _newUsernameController = TextEditingController();
   final TextEditingController _newPasswordController = TextEditingController();
-
-  final Authentication _auth = Authentication();
 
   @override
   Widget build(BuildContext context) {
@@ -28,79 +24,20 @@ class _RegisterPageState extends State<RegisterPage> {
           children: [
             TextField(
               controller: _newUsernameController,
-              decoration: const InputDecoration(labelText: 'New Username'),
+              decoration: const InputDecoration(labelText: 'Email'),
             ),
             const SizedBox(height: 16),
             TextField(
               controller: _newPasswordController,
-              decoration: const InputDecoration(labelText: 'New Password'),
+              decoration: const InputDecoration(labelText: 'Password'),
               obscureText: true,
             ),
             const SizedBox(height: 24),
             ElevatedButton(
               onPressed: ( )async {
-                await _auth.init();
-            dynamic result = await _auth.newUser(email: _newUsernameController.text, password: _newPasswordController.text);
-            if(result == 'success'){
-              Navigator.pushNamed(context, "/home");
-            }
-            else {
-              newDialog(context, result, "Attention");
-            }
-            },
-              child: const Text('Register'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-/*
-import 'package:flutter/material.dart';
-import '../services/authentication.dart';
-
-class RegisterPage extends StatefulWidget {
-  @override
-  _RegisterPageState createState() => _RegisterPageState();
-}
-
-class _RegisterPageState extends State<RegisterPage> {
-  final TextEditingController _newUsernameController = TextEditingController();
-  final TextEditingController _newPasswordController = TextEditingController();
-  final Authentication _auth = Authentication();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register Page'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _newUsernameController,
-              decoration: const InputDecoration(labelText: 'New Username'),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _newPasswordController,
-              decoration: const InputDecoration(labelText: 'New Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () async {
-                dynamic result = await _auth.newUser(
-                  email: _newUsernameController.text,
-                  password: _newPasswordController.text,
-                );
+                String result = await Manager.newUserManager(email: _newUsernameController.text, password: _newPasswordController.text);
                 handleRegistrationResult(result);
-              },
+            },
               child: const Text('Register'),
             ),
           ],
@@ -111,11 +48,9 @@ class _RegisterPageState extends State<RegisterPage> {
 
   void handleRegistrationResult(dynamic result) {
     if (result == 'success') {
-      Navigator.pushNamed(context, "/home");
+      Navigator.pushReplacementNamed(context, "/home");
     } else {
       newDialog(context, result, "Attention");
     }
   }
 }
-
- */
